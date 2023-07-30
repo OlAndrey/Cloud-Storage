f3350af84fcd656045be6a71d730fe8c28e9f813
OlAndrey <oleynik.andrey01@gmail.com>
2023-07-30 23:16:38 +0300

    Initial server setup

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const http = require('http')
+
+const app = express()
+dotenv.config()
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'http://localhost:5500']
+  })
+)
+
+const port = process.env.PORT || 3000
+const server = http.createServer(app)
+
+// Server Setup
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('MongoDB connected!')
+    server.listen(port, () => {
+      console.log(`Server started on port: ${port}`)
+    })
+  })
+  .catch((err) => {
+    console.log({ err })
+    process.exit(1)
+  })
